910e9b0b587a26b80a38680d4a243a15330b9da4
manish978973 <manishrchandran@gmail.com>
2020-09-18 13:33:29 +0200

    Update Jwtms1Application.java

--- src/main/java/com/example/demo/Jwtms1Application.java
+++ src/main/java/com/example/demo/Jwtms1Application.java
@@ -1,10 +1,12 @@
 package com.example.demo;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableRabbit
 public class Jwtms1Application {
 
@@ -13,3 +15,4 @@ public class Jwtms1Application {
 	}
 
 }
+
